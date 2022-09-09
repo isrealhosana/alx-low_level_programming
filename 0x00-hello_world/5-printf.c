57d80962a703a3d925f16c54c246027139c472a6
isreal hosana <isrealhosana40@gmail.com>
2022-09-09 06:35:11 -0700

    Task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammer, but the outcome is a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
