eb4d9dafa0d274940db0e964d82fdf8ce5d79521
isreal hosana <isrealhosana40@gmail.com>
2022-09-09 06:41:44 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammer, but the outcome is a piece of art," followed by a new line.
- *
+ * main - Entry point
  * Return: Always 0.
  */
 int main(void)
