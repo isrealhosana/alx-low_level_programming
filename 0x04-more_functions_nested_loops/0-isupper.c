7c9a8d03fa08766e3df025a83c99050032705767
isreal hosana <isrealhosana40@gmail.com>
2022-09-16 03:22:17 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,19 +2,12 @@
 /**
  * _isupper - checks for uppercase character
  * @c: variable text
- * Return: Always 0.
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-
-
-	if (c >= 'A' && c <= 'Z')
-	{
-	return (1);
-	}
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
 	else
-{
-	return (0);
-}
-
+		return (0);
 }
