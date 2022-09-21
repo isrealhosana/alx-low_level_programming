1f9c3a0e69fc5a45c45b4226eebd266cd3a15841
isreal hosana <isrealhosana40@gmail.com>
2022-09-21 12:36:07 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * reset_to_98 - a function that takes a pointer to an int as parameter
- * and updates the value it points to 98
- * @n: input
- * Return: n
+ * main -check the code
+ * 
+ * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
