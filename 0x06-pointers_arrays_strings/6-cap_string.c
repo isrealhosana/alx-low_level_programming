9e60e52d371d6681296d57e8d41416e10e3faac7
isreal hosana <isrealhosana40@gmail.com>
2022-09-23 01:56:24 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *n)
 {
 	int i, j;
 	int cap = 32;
-	int separators[] = {' ', '\t', ',', ';', '.', '!', '?', '"', '(',
+	int separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(',
 		')', '{', '}'};
 
 	for (i = 0; n[i] != '\0'; i++)
