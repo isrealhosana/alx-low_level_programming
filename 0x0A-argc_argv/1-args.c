01dba3af2766d4fa5d788e2857d61ae24b3d2003
isreal hosana <isrealhosana40@gmail.com>
2022-09-30 13:44:43 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, _attribute_((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
