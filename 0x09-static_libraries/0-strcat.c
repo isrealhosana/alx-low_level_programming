de2c2a77097e614bfa0a8253b4836b2d9b036ecc
isreal hosana <isrealhosana40@gmail.com>
2022-10-03 05:42:45 -0700

    0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - function that pthat concatenates two strigs.
+ * @dest: The string to be printed.
+ * @src: The string to be printd.
+ *
+ * Return: char *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int i, j;
+
+	for (i = 0; dest[i] != '\0';)
+		i++;
+
+	for (j = 0; (dest[i + j] = *src++) != '\0';)
+		j++;
+
+	return (dest);
+}
