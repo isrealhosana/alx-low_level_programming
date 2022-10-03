726907336991a9eba7e8c88e2af5e6bbe7495b1a
isreal hosana <isrealhosana40@gmail.com>
2022-10-03 07:59:53 -0700

    5-strstr.c

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring
+ * @haystack: string
+ * @needle: string
+ *
+ * Return: 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return (haystack);
+	}
+	return (0);
+}
