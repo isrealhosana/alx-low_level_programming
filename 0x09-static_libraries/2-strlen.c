b7885e81e25f5d73beaadd97f77baaa508560c0f
isreal hosana <isrealhosana40@gmail.com>
2022-10-03 06:18:32 -0700

    2-strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * _strlen - function that returns the length of a string.
+ * @s: The character to be checked.
+ *
+ * Return: i (the length of the string)
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0;)
+		i++;
+
+	return (i);
+}
